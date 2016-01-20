cc99430b7a67ecfb3a78c61e2485e40802f6988d
Paul Broadbent <dbdkmezz@gmail.com>
2016-01-20 20:22:57 +0000

    organised fucntions a little into objects

--- scraper.py
+++ scraper.py
@@ -2,60 +2,72 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
+ALL_HERO_NAMES_URL = "http://www.dota2.com/heroes/"
+
 class HeroAndAdvantage:
     name = ""
     advantage = ""
 
     def __init__(self, name, advantage):
-      self.name = name   
+      self.name = name
       self.advantage = advantage
 
 
-def has_data_link_to_attr(tag):
-    return tag.has_attr("data-link-to") 
+class AdvantageDataForAHero:
+    ADVANTAGES_URL_START = "http://www.dotabuff.com/heroes/"
+    ADVANTAGES_URL_END = "/matchups"
 
+    name = ""
+    data = []
 
-def has_style_attr(tag):
-    return tag.has_attr("style")
+    def __init__(self, name):
+        self.name = name
+        url = self.ADVANTAGES_URL_START + self.name_to_url_name(name) + self.ADVANTAGES_URL_END
+        web_content = load_url(url)
+        self.data = self.get_advantages_from_string(web_content)
+
+    @staticmethod
+    def name_to_url_name(name):
+        name = name.lower()
+        name = name.replace(" ", "-")
+        name = name.replace("'", "")
+        return name
+
+    @staticmethod
+    def get_advantages_from_string(content):
+        soup = BeautifulSoup(content, "html.parser")
+        soup = soup.find("table", class_="sortable")
+
+        list = []
+
+        for line in soup.find_all(AdvantageDataForAHero.has_data_link_to_attr):
+            name = line.find(class_="cell-xlarge").get_text()
+            advantage = AdvantageDataForAHero.get_num_from_percent(
+                line.find(string=re.compile("%")))
+            list.append(HeroAndAdvantage(name, advantage))
+
+        return list
+
+    @staticmethod
+    def has_data_link_to_attr(tag):
+        return tag.has_attr("data-link-to") 
+
+    @staticmethod
+    def get_num_from_percent(string):
+        string = string.replace("%", "")
+        if(len(string) == 0):
+            return 0
+        return float(string)
 
 
 def load_file(filename):
     with open(filename, "r") as content_file:
         return content_file.read()
 
-
 def load_url(url):
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
     r = requests.get(url, headers=headers)
     return r.text
-
-
-def get_num_from_percent(string):
-    string = re.sub(r"%", "", string)
-    if(len(string) == 0):
-        return 0
-    return float(string)
-
-
-def get_advantages_from_string(content):
-    soup = BeautifulSoup(content, "html.parser")
-    soup = soup.find("table", class_="sortable")
-
-    list = []
-
-    for line in soup.find_all(has_data_link_to_attr):
-        name = line.find(class_="cell-xlarge").get_text()
-        advantage = get_num_from_percent(
-            line.find(string=re.compile("%")))
-        list.append(HeroAndAdvantage(name, advantage))
-
-    return list
-
-
-def get_advantages_from_url(url):
-    file_string = load_url("http://www.dotabuff.com/heroes/disruptor/matchups")
-    return get_advantages_from_string(file_string)
-
 
 def get_hero_names_from_string(content):
     soup = BeautifulSoup(content, "html.parser")
@@ -63,15 +75,25 @@ def get_hero_names_from_string(content):
 
     list = []
 
-    for line in soup.find_all(has_style_attr):
-        list.append(line.get_text())
+    for line in soup.find_all("option"):
+        text = line.get_text()
+        if(text != "HERO NAME" and text != "All"):
+            list.append(text)
 
     return list
 
+def load_all_hero_data():
+    web_content = load_url(ALL_HERO_NAMES_URL)
+    hero_list = get_hero_names_from_string(web_content)
+    results = []
+    for hero in hero_list:
+        print("Loading " + hero)
+        results.append(AdvantageDataForAHero(hero))
+        
+    return results
+
+if __name__ == "__main__":
+    load_all_hero_data()
+    
 
 
-# list = get_advantages_from_url("http://www.dotabuff.com/heroes/disruptor/matchups")
-# for item in list:
-#     print("")
-#     print(item.name)
-#     print(item.advantage)
--- test_scraper.py
+++ test_scraper.py
@@ -7,7 +7,7 @@ import unittest
 class TestScrapingOfAdvantages(unittest.TestCase):
     def setUp(self):
         file_string = scraper.load_file("samples/Disruptor.html")
-        self.list = scraper.get_advantages_from_string(file_string)
+        self.list = scraper.AdvantageDataForAHero.get_advantages_from_string(file_string)
 
     def test_first_name(self):
         self.assertEqual(self.list[0].name, "Axe")
@@ -37,18 +37,33 @@ class TestGetHeroNames(unittest.TestCase):
         self.assertEqual(self.list.count("Disruptor"), 1)
 
 
-class TestHelperFunctions(unittest.TestCase):
+class TestGetNumFromPercent(unittest.TestCase):
     def test_get_num_from_percent(self):
         string = "1.8%"
-        self.assertEqual(scraper.get_num_from_percent(string), 1.8)
+        self.assertEqual(scraper.AdvantageDataForAHero.get_num_from_percent(string), 1.8)
 
     def test_get_num_from_percent_negative(self):
         string = "-1.8%"
-        self.assertEqual(scraper.get_num_from_percent(string), -1.8)
+        self.assertEqual(scraper.AdvantageDataForAHero.get_num_from_percent(string), -1.8)
 
     def test_get_num_from_percent_empty(self):
         string = ""
-        self.assertEqual(scraper.get_num_from_percent(string), 0)
+        self.assertEqual(scraper.AdvantageDataForAHero.get_num_from_percent(string), 0)
+
+
+class TestNameToUrlName(unittest.TestCase):
+    def test_blank(self):
+        self.assertEqual(scraper.AdvantageDataForAHero.name_to_url_name(""), "")
+
+    def test_one_word(self):
+        self.assertEqual(scraper.AdvantageDataForAHero.name_to_url_name("Disruptor"), "disruptor")
+
+    def test_four_words(self):
+        self.assertEqual(scraper.AdvantageDataForAHero.name_to_url_name("Keeper of the Light"), "keeper-of-the-light")
+
+    def test_apstrophe(self):
+        self.assertEqual(scraper.AdvantageDataForAHero.name_to_url_name("Nature's Prophet"), "natures-prophet")
+
 
 
 if __name__ == "__main__":
